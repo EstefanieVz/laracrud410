f9b1d2a623861d6313a0cc29d7a99af2e7a5f6ac
Estefanievz <23610289@utgz.edu.mx>
2024-10-25 14:04:51 -0600

    rutas

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,9 @@ Route::get('/indexProducts',[ProductController::class,'update {$store,$product}'
 Route::get('/indexProducts',[ProductController::class,'show'])->name('pshow'); //llama la primera funcion del controlador
 Route::get('/indexProducts',[ProductController::class,'destroy {$product}'])->name('pdestroy'); //llama la primera funcion del controlador
 
+
+Route::resource('/products',ProductController::class);//Es igual a las lineas que se ejecutaron antes del create,index,update,edit...etc.
+
 Route::get('/clients',function(){
     return view('clients_index');
 })-> name('clients');
