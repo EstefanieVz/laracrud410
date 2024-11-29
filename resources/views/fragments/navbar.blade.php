9cd212794dcd39226e7ba0ad573f2c63fdff97fe
Estefanievz <23610289@utgz.edu.mx>
2024-11-29 13:37:50 -0600

    Instalación de Bootstrap

--- resources/views/fragments/navbar.blade.php
+++ resources/views/fragments/navbar.blade.php
@@ -63,7 +63,7 @@
     
 
 </style>
-
+<!--
 <header>
     <nav>
         <p>
@@ -76,3 +76,37 @@
 </header>
 <br>
 <br>
+-->
+<!-- NAVBAR DE BOOSTRAP-->
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="{{route('index')}}">Dulces</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('index')}}">Inicio</a>
+              </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{route('products.index')}}">Productos</a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Información
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="{{route('clients.index')}}">Clientes</a></li>
+              <li><a class="dropdown-item" href="#">Direcciones</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="{{route('sales.index')}}">Ventas</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
